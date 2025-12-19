83105ec40a747242b87aa1b192761a17c881dafb
[supriya.stha.0601@gmail.com] <supriya.stha.0601@gmail.com>
2025-12-19 14:06:30 +0545

    Add Course model and update admin, views, and templates for CRUD functionality

--- crud_project/app/models.py
+++ crud_project/app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Course(models.Model):
+    image=models.ImageField(upload_to="images") #pip install pillow
+    title=models.CharField(max_length=20)
+    desc=models.TextField()
+    price=models.IntegerField()
--- crud_project/app/views.py
+++ crud_project/app/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 
 def show(request):
+    data=Course.objects.all()
     return render(request, 'home.html')
