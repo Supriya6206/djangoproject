9944c527346b78c8d1ca13a1ff3b5fb4a720a483
[supriya.stha.0601@gmail.com] <supriya.stha.0601@gmail.com>
2025-12-19 14:33:09 +0545

    Update home.html stylesheet link and enhance form handling in views.py

--- crud_project/app/views.py
+++ crud_project/app/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+from .models import Course
 
 # Create your views here.
 
 def show(request):
     data=Course.objects.all()
     return render(request, 'home.html')
+
+def form(request):
+    if request.method == "POST":
+        data = request.POST
+        n= data['name']
+        c=data['course']
+        e=data['email']
+        m=data['message']
+        var = student(name=n, course=c, email=e, message=m)
+        var.save()
+        messages.success(request, "Data saved successfully")
+        return redirect('form')
